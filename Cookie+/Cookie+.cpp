c7e6492c0eed76e58ae5afe9f2de5301269105fd
Zeale <a@infinityminers.com>
2017-09-23 14:52:48 -0500

    Added some framework things
    Added basic (programmatic) functions such as println and pause etc.

--- Cookie+/Cookie+.cpp
+++ Cookie+/Cookie+.cpp
@@ -2,10 +2,64 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
+#include <cstdio>
+
+namespace Cookiep {
+	using namespace std;
+
+	void parsecmd(string input);
+	void pause(bool printmsg);
+	void println(string line);
+	void prinln();
+
+	string getInput();
 
 
-int main()
-{
-    return 0;
+
+	void parsecmd(string input) {
+		if (input == "hello")println("hi.");
+		else
+			println("That command isn't registered...");
+
+	}
+
+	void pause(bool printmsg) {
+		if (printmsg)
+			println("Press any key to continue.");
+		getchar();
+	}
+
+	string getInput() {
+		string input;
+		cin >> input;
+		return input;
+	}
+
+	void println(string line) {
+		cout << line;
+		prinln();
+	}
+
+	void prinln() {
+		cout << "\n";
+	}
 }
 
+
+
+using namespace Cookiep;
+
+int main(int argc, char*args)
+{
+
+	bool run = true;
+	while (run) {
+		parsecmd(getInput());
+	}
+
+
+	pause(false);
+	return 0;
+}
